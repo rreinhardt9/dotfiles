77b4afc63996905f31c554f15867374efdd19ca1
Ross Reinhardt <rreinhardt9@gmail.com>
2017-02-25 15:02:09 -0500

    Cleaner atom package management

--- .atom/packages/rails-transporter/spec/fixtures/app/controllers/api/blogs_controller.rb
+++
@@ -1,7 +0,0 @@
-class Api::BlogsController < ApplicationController
-  # GET /blogs
-  # GET /blogs.json
-  def index
-    @blogs = Blog.all
-  end
-end
--- .atom/packages/rails-transporter/spec/fixtures/spec/factories/entry.rb
+++
@@ -1,7 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :entry, :class => 'Entry' do
-    title "hk's weblog's entry"
-  end
-end
